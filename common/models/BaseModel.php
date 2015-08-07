9e8ebbe6c85f9c114f2b8491eda0f20bfe24774b
Sergey Cherdantsev <liverenemy@gmail.com>
2015-08-07 16:23:48 +0700

    Base Model and Base Query classes

---
+++ common/models/BaseModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace common\models;
+use yii\db\ActiveRecord;
+
+/**
+ * Base model
+ */
+
+class BaseModel extends ActiveRecord
+{
+
+}
---
+++ common/models/query/BaseQuery.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace common\models\query;
+use yii\db\ActiveQuery;
+
+/**
+ * Base query class
+ */
+class BaseQuery extends ActiveQuery
+{
+
+}
