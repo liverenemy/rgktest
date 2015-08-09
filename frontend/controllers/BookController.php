f5ed6c66dd687e90c1596cb246d89a1dfa684586
Sergey Cherdantsev <liverenemy@gmail.com>
2015-08-10 00:43:44 +0700

    Book Controller - Authenticated users only

--- frontend/controllers/BookController.php
+++ frontend/controllers/BookController.php
@@ -19,6 +19,17 @@ class BookController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => \yii\filters\AccessControl::className(),
+                'rules' => [
+                    // allow authenticated users
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    // everything else is denied
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
